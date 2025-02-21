5c10af0ebaedc1d43ca70232a022328952e93ce5
Piyush Satija <piyush.satija@gmail.com>
2025-02-22 03:47:46 +0530

    feat: add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 import DataGrid from "./components/DataGrid/";
 
 function App() {
-  return <DataGrid />;
+  return (
+    <>
+      <DataGrid />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
